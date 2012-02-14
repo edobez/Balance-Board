1e0a3551516a65fdc4159ce31c148b0a840bfd21
Edoardo Bezzeccheri <coolman3@gmail.com>
2012-02-14 17:44:50 +0100

    Creato nuovo tipo di Menu

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -6,136 +6,76 @@ namespace Menu
 {
     class MenuManager
     {
-        /// <summary>
-        /// Root menu
-        /// </summary>
-        private MenuItem _rootMenu;
-        private int _currentMenuIndex;
-        private int _currentSubMenuIndex;
+        private int m_currentMenuIndex;
+        private int m_currentSubMenuIndex;
+        private MenuItem m_currentMenu;
+        private MenuItem m_currentSubMenu;
+        private Hashtable m_menuList;
+        private int m_menuCount;
+        private int[] m_subMenuCount;
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
         public MenuManager()
         {
-            _rootMenu = new MenuItem("Root");
+            m_menuList = new Hashtable();
+            m_currentMenuIndex = 1;
+            m_currentSubMenuIndex = 1;
+            m_subMenuCount = new int[10];
         }
 
-        public MenuItem Root
+        public void addMenu(MenuItem item, int id)
         {
-            get { return _rootMenu; }
+            m_menuList.Add(id, item);
+            m_menuCount++;
         }
 
-        /// <summary>
-        /// Handler of button Enter pressed
-        /// </summary>
-        public void butEnter()
+        public void addSubMenu(MenuItem item, int id)
         {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        /// Handler of button Menu pressed
-        /// </summary>
-        public void butMenu()
-        {
-            _currentMenuIndex++;
-            if (_currentMenuIndex > Root.SubMenuCount) _currentMenuIndex = 0;
-        }
-
-        /// <summary>
-        /// Handler of button Up pressed
-        /// </summary>
-        public void butUp()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        /// <summary>
-        /// Handler of button Down pressed
-        /// </summary>
-        public void butDown()
-        {
-            throw new System.NotImplementedException();
+            int temp = id / 10;
+            m_menuList.Add(id, item);
+            m_subMenuCount[temp]++;
         }
 
         public void display()
         {
-            MenuItem currentMenu = Root.getChild(_currentMenuIndex);
-            MenuItem currentSubMenu = Root.getChild(_currentMenuIndex).getChild(_currentSubMenuIndex);
+            int id = m_currentMenuIndex * 10 + m_currentSubMenuIndex;
+            int id_top = m_currentMenuIndex * 10;
 
-            Debug.Print(currentMenu.Name);
-            if (currentSubMenu.Param.Length > 0) Debug.Print(currentSubMenu.Name + " " + currentSubMenu.Param);
-            else Debug.Print(currentSubMenu.Name);
+            if (m_menuList.Contains(id))
+            {
+                MenuItem item_top = m_menuList[id_top] as MenuItem;
+                MenuItem item = m_menuList[id] as MenuItem;
+
+                Debug.Print(item_top.Name);
+                Debug.Print(item.Name);
+            }
         }
     }
 
     public class MenuItem
     {
-        private ArrayList _childMenu;
-        private MenuItem _parentMenu;
-        private Object _param;
-        private String _name;
-        private int _level;
-        private bool _isEditable;
+        private Object m_param;
+        private String m_name;
+        private bool m_isEditable;
 
-        /// <summary>
-        /// Constructor for menu
-        /// </summary>
-        /// <param item="item">Name of the menu</param>
-        /// <param item="param">Object to show</param>
-        /// <param item="editable">Tells if the object is editable</param>
-        public MenuItem(string name, object param, bool editable)
+        public MenuItem(string name, object param, bool isEditable)
         {
-            _childMenu = new ArrayList();
-            _name = name;
-            _param = param;
-            _isEditable = editable;
+            m_name = name;
+            m_param = param;
+            m_isEditable = isEditable;
         }
 
-        /// <summary>
-        /// Constructor for name-only menu
-        /// </summary>
-        /// <param item="item">Name of the menu</param>
-        public MenuItem(string name) : this(name, null, false)
-        {
-        }
-
-        /// <summary>
-        /// Constructor for root
-        /// </summary>
-        public MenuItem() : this("Root")
-        {
-            _level = 0;
-        }
+        public MenuItem(string name)
+            : this(name, null, false)
+        { }
 
         public string Name
         {
-            get { return _name; }
-            set { _name = value; }
+            get { return m_name; }
         }
 
-        public int SubMenuCount
+        public object Param
         {
-            get { return _childMenu.Count; }
-        }
-
-        public string Param
-        {
-            get { return _param.ToString(); }
-        }
-
-        public MenuItem addMenu(MenuItem item)
-        {
-            _childMenu.Add(item);
-            item._parentMenu = this;
-            item._level = this._level + 1;
-            return this;
-        }
-
-        public MenuItem getChild(int index)
-        {
-            return (_childMenu[index] as MenuItem);
+            get { return m_param; }
         }
     }
 }
--- Menu/Program.cs
+++ Menu/Program.cs
@@ -17,23 +17,21 @@ namespace Menu
             int caio = 7;
 
             MenuManager menu = new MenuManager();
-            MenuItem item0 = new MenuItem("ADC");
-            MenuItem item1 = new MenuItem("Angoli");
-            MenuItem item2 = new MenuItem("PID");
 
-            MenuItem item00 = new MenuItem("CH0:", pippo, true);
-            MenuItem item01 = new MenuItem("CH1:", tizio, true);
-            MenuItem item02 = new MenuItem("CH1:", caio, true);
+            menu.addMenu(new MenuItem("ADC"), 10);
+            menu.addSubMenu(new MenuItem("CH0:", pippo, true), 11);
+            menu.addSubMenu(new MenuItem("CH1:", tizio, true), 12);
+            menu.addSubMenu(new MenuItem("CH2:", caio, true), 13);
 
-            menu.Root.addMenu(item0);
-            menu.Root.addMenu(item1);
-            menu.Root.addMenu(item2);
-            item0.addMenu(item00).addMenu(item01).addMenu(item02);
+            menu.addMenu(new MenuItem("PID"), 20);
+            menu.addSubMenu(new MenuItem("CH0:", pippo, true), 21);
+
+            menu.addMenu(new MenuItem("Output"), 30);
+            menu.addSubMenu(new MenuItem("CH0:", pippo, true), 31);
 
             while (true)
             {
                 menu.display();
-                menu.butMenu();
             }
         }
 
